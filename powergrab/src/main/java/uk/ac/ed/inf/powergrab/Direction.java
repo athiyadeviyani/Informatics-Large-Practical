4cbf9e20d68ce12e345bd7ab9a24863fdeeecbbf
Athiya Deviyani <athiyadee@gmail.com>
2019-09-27 13:58:02 +0100

    Add files via upload

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/Direction.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Direction.java
@@ -1,6 +1,7 @@
 package uk.ac.ed.inf.powergrab;
 
 public enum Direction {
+	// Define constants for all sixteen compass directions
 	N, NNE, NE, ENE, E, ESE, SE, SSE, 
 	S, SSW, SW, WSW, W, WNW, NW, NNW;
 }
